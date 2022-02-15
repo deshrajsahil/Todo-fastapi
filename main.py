479f26d212f9b771be7fa502310c28c345b49d18
deshraj <drs@Deshrajs-MacBook-Air.local>
2022-02-15 17:13:39 +0530

    Basic todo with CRUD operations

---
+++ main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI,HTTPException,status
+from pydantic import BaseModel
+from typing import List,Optional
+
+class Todo(BaseModel):
+    text_field: str
+    due_date: str
+    is_completed: str
+    assigned_to: str
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return "hello world!!"
+
+# CRED operations
+
+store_todo = []  #to store todo list
+
+@app.post('/todo/', tags=['create Todo'])
+def create_todo(todo: Todo):
+    store_todo.append(todo)
+    return todo
+
+@app.get('/todo/',response_model=List[Todo], tags=['Show all Todo'])
+def get_all_todos():
+    return store_todo
+
+@app.get('/todo/{id}', tags=['Show specific Todo'])
+def get_todo(id: int):
+    try:
+        return store_todo[id]
+    except:
+        raise HTTPException( status_code=status.HTTP_404_NOT_FOUND, detail="ToDo doesn't exist")
+
+@app.put('/todo/{id}', tags=['Update Todo'])
+def update_todo(id: int, todo: Todo):
+    try:
+        store_todo[id] = todo
+        return store_todo[id]
+    except:
+        raise HTTPException( status_code=status.HTTP_404_NOT_FOUND, detail="ToDo not found")
+
+
+@app.delete('/todo/{id}', tags=['Delete Todo'])
+def delete_todo(id:int):
+    try:
+        obj = store_todo[id]
+        store_todo.pop(id)
+        return obj
+    except:
+        raise HTTPException( status_code=status.HTTP_404_NOT_FOUND, detail="ToDo not found")
+
+    
