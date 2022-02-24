5b5e109fea06c86052e915e3ef437c3404e38944
Deshraj <sahildeshraj98.com>
2022-02-24 10:54:22 +0530

    backend is working properly

--- main.py
+++ main.py
@@ -5,11 +5,21 @@ from sqlalchemy.sql.functions import user
 from db.database import engine
 from auth import authentication
 from routers import user,todo
+from fastapi.middleware.cors import CORSMiddleware
 
 
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+
+)
 
 app.include_router(user.router)
 app.include_router(todo.router)
